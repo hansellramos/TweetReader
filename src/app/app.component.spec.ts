97d743c30f7f04ee6e3ee8acaa0ffc4541d57e3d
Hansel Ramos <hansell.ramos@gmail.com>
2018-09-04 22:16:15 -0500

    Fixed unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {HeaderFormComponent} from './components/header-form/header-form.component';
+import {TweetFormComponent} from './components/tweet-form/tweet-form.component';
+import {FormsModule} from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         HeaderFormComponent,
+        TweetFormComponent
       ],
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -15,7 +21,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'TweetsReader'`, async(() => {
+  it(`should have as title 'Tweets Reader'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Tweets Reader');
--- src/app/components/tweet-form/tweet-form.component.spec.ts
+++ src/app/components/tweet-form/tweet-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TweetFormComponent } from './tweet-form.component';
+import { Tweet } from '../../models/tweet';
+import { FormsModule } from '@angular/forms';
+import {AppComponent} from '../../app.component';
 
 describe('TweetFormComponent', () => {
   let component: TweetFormComponent;
@@ -8,7 +11,8 @@ describe('TweetFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TweetFormComponent ]
+      declarations: [ TweetFormComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   }));
@@ -21,5 +25,23 @@ describe('TweetFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have default values', () => {
+    fixture = TestBed.createComponent(TweetFormComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.tweet.videoUrl).toEqual('');
+    expect(app.tweet.comment).toEqual('');
+    expect(app.tweet.content).toEqual('');
+  });
+  it('should change when modify tweet videoUrl and/or comment attributes', () => {
+    fixture = TestBed.createComponent(TweetFormComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.tweet.videoUrl = 'youtube.com/testing';
+    expect(app.tweet.content).toEqual('youtube.com/testing');
+    app.tweet.videoUrl = '';
+    app.tweet.comment = 'Hello World';
+    expect(app.tweet.content).toEqual('Hello World');
+    app.tweet.videoUrl = 'youtube.com/testing';
+    expect(app.tweet.content).toEqual('Hello World youtube.com/testing');
   });
 });
